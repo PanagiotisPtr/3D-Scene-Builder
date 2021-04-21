1c637903a5a1bc64610c7868d2f3bcc0c057ced6
Panagiotis Petridis <lpanagiotispetridisl@gmail.com>
2021-04-21 21:42:22 +0100

    Added plane object

---
+++ SceneBuilder/Plane.h
@@ -0,0 +1,39 @@
+#ifndef OBJECT_PLANE_H
+#define OBJECT_PLANE_H
+
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#endif
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "glm/vec3.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtx/string_cast.hpp"
+#include "glm/common.hpp"
+
+#include "Editable.h"
+
+class Plane : public Editable {
+public:
+	Plane(glm::vec3 p, glm::vec3 c) : Editable(p, c) {}
+
+protected:
+	void drawShape() const override {
+		glBegin(GL_TRIANGLES);
+
+		glVertex3f(-1, 0, 1);
+		glVertex3f(1, 0, 1);
+		glVertex3f(1, 0, -1);
+
+		glVertex3f(1, 0, -1); 
+		glVertex3f(-1, 0, -1);
+		glVertex3f(-1, 0, 1);
+
+		glEnd();
+	}
+};
+
+#endif
